77c1d9fc56f87583e9cff338fd77fa270858d4e9
refaelsh <refaelsh@gmail.com>
2017-09-10 21:53:04 +0300

    Code formating

--- main.c
+++ main.c
@@ -6,46 +6,44 @@
 
 #define MAXSTR  1024
 
-static void XSetRoot(const char *name){
-        Display *display;
+static void XSetRoot(const char *name) {
+	Display *display;
 
-        if (( display = XOpenDisplay(0x0)) == NULL ) {
-                fprintf(stderr, "[barM] cannot open display!\n");
-                exit(1);
-        }
+	if ((display = XOpenDisplay(0x0)) == NULL) {
+		fprintf(stderr, "[barM] cannot open display!\n");
+		exit(1);
+	}
 
-        XStoreName(display, DefaultRootWindow(display), name);
-        XSync(display, 0);
+	XStoreName(display, DefaultRootWindow(display), name);
+	XSync(display, 0);
 
-        XCloseDisplay(display);
+	XCloseDisplay(display);
 }
 
-int main(void)
-{
+int main(void) {
 	char status[MAXSTR];
 
+	long double a[4], b[4], loadavg;
+	FILE *fp;
+	char dump[50];
 
-    long double a[4], b[4], loadavg;
-    FILE *fp;
-    char dump[50];
+	for (;;) {
+		fp = fopen("/proc/stat", "r");
+		fscanf(fp, "%*s %Lf %Lf %Lf %Lf", &a[0], &a[1], &a[2], &a[3]);
+		fclose(fp);
+		sleep(1);
 
-    for(;;)
-    {
-        fp = fopen("/proc/stat","r");
-        fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&a[0],&a[1],&a[2],&a[3]);
-        fclose(fp);
-        sleep(1);
+		fp = fopen("/proc/stat", "r");
+		fscanf(fp, "%*s %Lf %Lf %Lf %Lf", &b[0], &b[1], &b[2], &b[3]);
+		fclose(fp);
 
-        fp = fopen("/proc/stat","r");
-        fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&b[0],&b[1],&b[2],&b[3]);
-        fclose(fp);
+		loadavg = ((b[0] + b[1] + b[2]) - (a[0] + a[1] + a[2]))
+				/ ((b[0] + b[1] + b[2] + b[3]) - (a[0] + a[1] + a[2] + a[3]));
+		printf("The current CPU utilization is : %Lf\n", loadavg);
 
-        loadavg = ((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3]));
-        printf("The current CPU utilization is : %Lf\n",loadavg);
+		snprintf(status, MAXSTR, "%Lf", loadavg);
+		XSetRoot(status);
+	}
 
-        snprintf(status, MAXSTR, "%Lf", loadavg);
-        XSetRoot(status);
-    }
-
-    return(0);
+	return (0);
 }
